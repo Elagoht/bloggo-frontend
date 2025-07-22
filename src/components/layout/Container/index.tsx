0a54ee936ae572e027662ec8e92bd60cbc2d8412
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-07-22 15:28:35 +0300

    feat: Add Container component for responsive layout with customizable sizes

---
+++ src/components/layout/Container/index.tsx
@@ -0,0 +1,26 @@
+import classNames from "classnames";
+import { ParentComponent } from "solid-js";
+
+type ContainerProps = {
+  size?: "sm" | "md" | "lg" | "xl";
+};
+
+const Container: ParentComponent<ContainerProps> = ({
+  size = "lg",
+  children,
+}) => {
+  return (
+    <div
+      class={classNames("flex flex-col gap-1 mx-auto w-full", {
+        "max-w-screen-sm": size == "sm",
+        "max-w-screen-md": size == "md",
+        "max-w-screen-lg": size == "lg",
+        "max-w-screen-xl": size == "xl",
+      })}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
