26fe0b96f5a8c0d6cbfdac7a3d00a19956c4fa2f
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-07-23 17:05:42 +0300

    feat: Implement lazy loading for routes in index.tsx to improve performance

--- src/index.tsx
+++ src/index.tsx
@@ -3,32 +3,46 @@ import "./design/index.css";
 import { render } from "solid-js/web";
 
 import { Route, Router } from "@solidjs/router";
-import AuthLayout from "./app/auth/layout";
-import LoginPage from "./app/auth/login/page";
-import DashboardPage from "./app/panel/dashboard/page";
-import PanelLayout from "./app/panel/layout";
-import ProfilePage from "./app/panel/profile/page";
-import CategoriesPage from "./app/panel/categories/page";
-import AuthGuardLayout from "./app/panel/guard";
-import CategoryCreatePage from "./app/panel/categories/create/page";
-import CategoryEditPage from "./app/panel/categories/edit/[slug]/page";
+import { lazy } from "solid-js";
 
 render(
   () => (
     <Router>
-      <Route path="/auth" component={AuthLayout}>
-        <Route path="/login" component={LoginPage} />
+      <Route path="/auth" component={lazy(() => import("./app/auth/layout"))}>
+        <Route
+          path="/login"
+          component={lazy(() => import("./app/auth/login/page"))}
+        />
       </Route>
 
-      <Route path="/" component={AuthGuardLayout}>
-        <Route path="/" component={PanelLayout}>
-          <Route path="/" component={DashboardPage} />
-          <Route path="/profile" component={ProfilePage} />
+      <Route path="/" component={lazy(() => import("./app/panel/guard"))}>
+        <Route path="/" component={lazy(() => import("./app/panel/layout"))}>
+          <Route
+            path="/"
+            component={lazy(() => import("./app/panel/dashboard/page"))}
+          />
+          <Route
+            path="/profile"
+            component={lazy(() => import("./app/panel/profile/page"))}
+          />
 
           <Route path="/categories">
-            <Route path="/" component={CategoriesPage} />
-            <Route path="/create" component={CategoryCreatePage} />
-            <Route path="/edit/:slug" component={CategoryEditPage} />
+            <Route
+              path="/"
+              component={lazy(() => import("./app/panel/categories/page"))}
+            />
+            <Route
+              path="/create"
+              component={lazy(
+                () => import("./app/panel/categories/create/page")
+              )}
+            />
+            <Route
+              path="/edit/:slug"
+              component={lazy(
+                () => import("./app/panel/categories/edit/[slug]/page")
+              )}
+            />
           </Route>
         </Route>
       </Route>
