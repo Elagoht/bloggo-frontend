1418a656de73b7342d915771c47fb2e6310fa02e
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-07-23 17:55:33 +0300

    style: Enhance H3 component with additional text color classes for improved accessibility

--- src/components/typography/H3.tsx
+++ src/components/typography/H3.tsx
@@ -1,7 +1,11 @@
 import { ParentComponent } from "solid-js";
 
 const H3: ParentComponent = ({ children }) => {
-  return <h3 class="text-lg font-bold">{children}</h3>;
+  return (
+    <h3 class="text-lg font-bold text-smoke-700 dark:text-smoke-300">
+      {children}
+    </h3>
+  );
 };
 
 export default H3;
