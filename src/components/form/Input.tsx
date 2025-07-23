12eeeb57a35a2194d8d4382c16b1818e03cdb253
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-07-23 15:42:17 +0300

    style: Update Input component to include placeholder styling for better user experience

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -10,7 +10,7 @@ const Input: Component<InputProps> = ({ label, ...props }) => {
     <input
       {...props}
       class={classNames(
-        "h-8 p-2 rounded text-sm bg-smoke-50 text-smoke-950 dark:bg-smoke-900 dark:text-smoke-100 transition-all ease-in-out focus:outline-none focus:outline-smoke-300 dark:focus:outline-smoke-700",
+        "h-8 p-2 rounded text-sm bg-smoke-50 text-smoke-950 dark:bg-smoke-900 dark:text-smoke-100 transition-all ease-in-out focus:outline-none focus:outline-smoke-300 dark:focus:outline-smoke-700 placeholder:text-smoke-500",
         props.class
       )}
     />
